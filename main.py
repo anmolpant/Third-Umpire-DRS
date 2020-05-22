49d696322493dde522775a5eab2ed045f8f96ab9
Anmol Pant <anmolpant@gmail.com>
2020-05-22 23:43:03 +0530

    added all buttons

--- main.py
+++ main.py
@@ -14,4 +14,24 @@ canvas = tkinter.Canvas(window, width=SET_WIDTH, height = SET_HEIGHT)
 photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
 image_on_canvas = canvas.create_image(0,0, anchor = tkinter.NW, image=photo )
 canvas.pack()
+
+
+#adding buttons
+btn = tkinter.Button(window, text = "<< Previous (fast)", width = 50)
+btn.pack()
+
+btn = tkinter.Button(window, text = "<< Previous (slow)", width = 50)
+btn.pack()
+
+btn = tkinter.Button(window, text = " Next (slow) >>", width = 50)
+btn.pack()
+
+btn = tkinter.Button(window, text = " Next (fast) >>", width = 50)
+btn.pack()
+
+btn = tkinter.Button(window, text = " Give Out", width = 50)
+btn.pack()
+
+btn = tkinter.Button(window, text = " Give Not Out", width = 50)
+btn.pack()
 window.mainloop()
