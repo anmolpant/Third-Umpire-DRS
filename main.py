fb5873b17c9b6b44b89f933aeb00e491a4546a4b
Anmol Pant <anmolpant@gmail.com>
2020-05-24 23:35:05 +0530

    added decision pending screen

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import tkinter
 import cv2
 from functools import partial
 import PIL.Image, PIL.ImageTk
+import imutils
 import threading
 
 def play(speed):
     print(f"inshallah bois. speed is {speed} ")
 
-def pending():
-    pass
+def pending(decision):
+    #display decision pending image
+    frame = cv2.cvtColor(cv2.imread("pending.png"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    #wait
+    #sponsor image
+    #wait
+    #decision
 
 def out():
     thread = threading.Thread(target=pending, args=("out",))
